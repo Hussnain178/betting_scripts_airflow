f18ebcae7b481904e4010d7d83340e67efe65528
mrfaizan786 <faizanriaz607@gmail.com>
2025-08-23 18:57:40 +0500

    new updates

--- bovada.py
+++ bovada.py
@@ -444,11 +444,8 @@ class BovadaOddsSpider(scrapy.Spider):
                 header_result = check_header_name(standardized_market_name)
                 market_header_category = header_result[0]
                 final_market_name = header_result[1]
-                if 'o/u' in market_name.lower() and '-' in market_name.lower():
-                    check_list=['set','half','period','quarter','inning','game','odd','match','extra','overtime']
-                    for check in check_list:
-                        if check not in market_name.lower():
-                            continue
+                if 'o/u' in final_market_name.lower() and '-' in final_market_name.lower():
+                    continue
                 self.unique_odds_keys.add(final_market_name)
 
                 # Initialize nested structure
